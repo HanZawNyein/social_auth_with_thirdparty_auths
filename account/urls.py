f9a529e3bc966fe67242625349ff3b257742139e
HanZawNyein <hanzawnyineonline@gmail.com>
2022-12-03 11:41:13 +0630

    add login and logout view of django auth framework

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     # django auth fromework default views
     path('login/',auth_views.LoginView.as_view(),name="login"),
-    path('logout/',auth_views.LogoutView.as_view(),name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('',views.dashboard,name='dashboard')
 ]
